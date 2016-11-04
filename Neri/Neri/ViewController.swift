59fc5306b2e6d155c0dfa7f24de8370bc627ac28
Pedro de Sá <pedro.desa@live.com>
2016-11-04 15:02:21 -0200

    gitignore

---
+++ Neri/Neri/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Neri
+//
+//  Created by Pedro de Sá on 04/11/16.
+//  Copyright © 2016 Pedro de Sá. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
