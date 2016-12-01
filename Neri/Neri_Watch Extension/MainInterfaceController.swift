84306a4553ce6f9842690af649aa4bac6d6fb958
cardoso-ana <AnaCardoso@Ana-Cardosos-MacBook-Pro-2.local>
2016-12-01 16:20:16 -0200

    Interface Watch

---
+++ Neri/Neri_Watch Extension/MainInterfaceController.swift
@@ -0,0 +1,17 @@
+//
+//  MainInterfaceController.swift
+//  Neri
+//
+//  Created by Ana Carolina Nascimento on 01/12/16.
+//  Copyright © 2016 Pedro de Sá. All rights reserved.
+//
+
+import WatchKit
+import HealthKit
+import CloudKit
+import CoreMotion
+
+
+class MainInterfaceController: WKInterfaceController {
+    
+}
